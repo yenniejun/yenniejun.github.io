5d140f78d05f78ef47ea6f76e20529953ba6e26a
Yennie Jun <yennie.jun@gmail.com>
2020-05-27 01:24:52 -0500

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,23 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Yennie Jun's Personal Website`,
+    description: `Yennie Jun's personal website.`,
+    author: `Yennie Jun`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    // "gatsby-plugin-catch-links", 
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog`,
+        path: `${__dirname}/src/content`,
+      },
+    },
+    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
