f2f9ba0649f4e87831f6bbba77b8897cabaf605d
Yennie Jun <yennie.jun@gmail.com>
2021-05-16 00:43:43 +0100

    Updates

---
+++ component---src-pages-contact-js-1cd2b01df864ad4c9020.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{"Cuy+":function(t,e,a){"use strict";a.r(e);var n=a("q1tI"),r=a.n(n),c=a("Bl7J"),i=a("vrFN");e.default=function(){return r.a.createElement(c.a,{title:"Contact"},r.a.createElement(i.a,{title:"Contact"}),r.a.createElement("div",{style:{maxWidth:"300px",marginBottom:"1.45rem"}}))}}}]);
+//# sourceMappingURL=component---src-pages-contact-js-1cd2b01df864ad4c9020.js.map
