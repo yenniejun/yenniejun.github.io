51e4fb848207131829d4b460ac15dbced4c35d46
Yennie Jun <yennie.jun@gmail.com>
2021-05-16 00:39:28 +0100

    Update the portfolio layout by adding images and tags

--- src/components/portfolio/panel.js
+++ src/components/portfolio/panel.js
@@ -1,14 +1,46 @@
 // Gatsby supports TypeScript natively!
 import React from "react"
+import Img from 'gatsby-image';
 import panelStyles from "./panel.module.css"
+import linkStyles from "./../posts/post-link.module.css"
 import { FaGithub } from 'react-icons/fa'
+import { IoIosPaper } from 'react-icons/io'
+import { RiArticleLine } from 'react-icons/ri'
+import { GiClick } from 'react-icons/gi'
+import { GoTag } from 'react-icons/go'
 
 const PortfolioPanel = (props) => {
-
     return (
       <>
         <div className={panelStyles.panel} style={{backgroundColor: props.color}}>
-            <h2>{props.data.title}</h2> 
+
+
+            <div className={panelStyles.projectTitle}>
+              {props.image && props.image.node &&
+              <div className={panelStyles.projectIconContainer}>
+                <Img 
+                  className={panelStyles.projectIcon}
+                  fluid={props.image.node.childImageSharp.fluid}
+                  alt={"image for" + props.image.node.base}
+                />
+
+              </div>
+                
+              } 
+              <div>
+                <h2>{props.data.title}</h2> 
+                {props.data.tags && 
+                  <div>{props.data.tags.map((item, i) => 
+                    <div key={i} className={linkStyles.blogtag}>
+                      <GoTag/>{item}
+                    </div>)}
+                  </div>
+                }
+              </div>
+            </div>
+            
+
+            
             <hr/>
             {props.data.description && 
               props.data.description.map((item, i) =>
@@ -20,20 +52,39 @@ const PortfolioPanel = (props) => {
               <h3>{props.data.announcement}</h3>
             }
 
-            {props.data.paperlink &&
-              <h3><a className={panelStyles.demolink} href={props.data.paperlink} target="_blank" rel="noreferrer"><span>Link to paper</span></a></h3>
-            }
+            <div className={panelStyles.codelinkContainer}>
+              {props.data.paperlink &&
+                <a className={panelStyles.codelink} href={props.data.paperlink} target="_blank" rel="noreferrer">
+                  <IoIosPaper />
+                  <span>Link to paper</span>
+                </a>
+              }
 
-            {props.data.demolink &&
-              <h3><a className={panelStyles.demolink} href={props.data.demolink} target="_blank" rel="noreferrer"><span>Demo</span></a></h3>
-            }
+              {props.data.articlelink &&
+                  <a className={panelStyles.codelink} href={props.data.articlelink} target="_blank" rel="noreferrer">
+                    <RiArticleLine />
+                    <span>Link to article</span>
+                  </a>
+                }
 
-            {props.data.codelink &&
-              <a className={panelStyles.codelink} href={props.data.codelink} target="_blank" rel="noreferrer">
-                <FaGithub/>
-                <span>Link to code</span>
-              </a>
-            }
+              {props.data.demolink &&
+                <a className={panelStyles.codelink} href={props.data.demolink} target="_blank" rel="noreferrer">
+                  <GiClick />
+                  <span>Link to demo</span>
+                </a>
+              }
+
+              {props.data.codelink &&
+                <a className={panelStyles.codelink} href={props.data.codelink} target="_blank" rel="noreferrer">
+                  <FaGithub/>
+                  <span>Link to code</span>
+                </a>
+              } 
+            </div>
+
+            
+
+            
         </div>
       </>
     )
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -3,9 +3,30 @@ import Layout from "../components/layout"
 import PortfolioPanel from "../components/portfolio/panel"
 import SEO from "../components/seo"
 import portfolioData from "../data/portfolio.json"
+import { useStaticQuery, graphql } from "gatsby"
 
-const PortfolioPage = () => (
-  <Layout 
+const PortfolioPage = () => {
+  const query = useStaticQuery(graphql`
+    query {
+      images: allFile(filter: {
+        relativeDirectory: {eq: "projects"}
+      }) {
+        edges {
+          node { 
+            base
+            id
+            childImageSharp {
+              fluid(maxWidth: 500, maxHeight: 500) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  return <Layout 
     title="Projects" 
     icon="computer"
     quote="Learning is movement from moment to moment"
@@ -23,11 +44,12 @@ const PortfolioPage = () => (
                 key={i} 
                 color = {"#f9f9f9"}
                 data={item}
+                image={query.images.edges.find(n => n.node.base === item.image)}
               />
             )
           }
     </div>
   </Layout>
-)
+}
 
 export default PortfolioPage
