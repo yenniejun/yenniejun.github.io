ae99a8d137aafa466f1b3a7065189bc8aba991a7
Yennie Jun <yennie.jun@gmail.com>
2021-09-06 10:45:08 -0400

    update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,9 @@ import { useStaticQuery, graphql } from "gatsby"
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
     query {
-      file(relativePath: { eq: "me_b&w.png" }) {
+      file(relativePath: { eq: "me_fb.jpeg" }) {
         childImageSharp {
-          fluid(maxWidth: 300, maxHeight: 300) {
+          fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
           }
         }
@@ -20,26 +20,31 @@ const IndexPage = () => {
     }
   `);
 
-  return <Layout title="Hi, I'm Yennie" icon="sunflower">
+  return <Layout title="Hi, I'm Yennie" subtitle="Data Scientist | AI Researcher | Writer" icon="sunflower">
     <SEO title="Yennie Jun" />
-    <div className={classnames("homeContainer", "mainContainer")}>
-      <br/>
-      <div className="home-blurb">I am a multi-disciplinary data scientist, AI researcher, and writer. On the technology side, I enjoy using data to ask important and difficult questions about human society. On the creative side, I love to write creative fiction, learn new instruments, and combine movements ranging from martial arts to dancing.</div>
-      <div className="homeFlex">
-        <div className="homeFlex-image">
-          <Img fluid={data.file.childImageSharp.fluid} alt="This is a picture of my face"/>
-        </div>
-        <div className="homeFlex-bio">
-          <p>I am currently studying for my Master of Science (MSc) in <a href="https://www.ox.ac.uk/admissions/graduate/courses/msc-social-data-science" className="normalLink" target="_blank" rel="noreferrer">Social Data Science</a> at the Oxford Internet Institute. I am also an AI and Data Science Research Fellow with the <a href="https://www.unglobalpulse.org/" target="_blank" className="normalLink" rel="noreferrer">UN Global Pulse</a>, where I am analyzing content shared on public radio across the African continent and <a href="https://www.unglobalpulse.org/2021/05/who-and-un-global-pulse-are-building-a-social-listening-radio-tool-to-aid-the-covid-19-infodemic-response/" target="_blank" className="normalLink" rel="noreferrer">developing social listening tools</a> to support monitoring the COVID-19 infodemic. </p>
-            
-          <p>I am a part of the <a href="http://bigdatastudies.net/" className="normalLink" target="_blank" rel="noreferrer">Big Data Studies Lab</a> at Seoul National University (SNU) as well as a student researcher with the <a href="https://oxai.org/labs" target="_blank" className="normalLink" rel="noreferrer">Oxford AI Labs</a>.</p>
 
-          <p>Before that, I was a software engineer at Microsoft on the Education team working on <a href="https://www.microsoft.com/en-us/education" className="normalLink" target="_blank" rel="noreferrer">education technology platforms</a> to empower students and teachers. Before that, I graduated from Tufts University with a B.S. in computer science, B.A. in history, and minor in music.</p>
-          
-          {/* <p>For fun, I love to cycle around the English countryside, hike, do martial arts (Taekwondo), read, and write (anything from short stories to fiction to poetry; check out my Writing!). I also love to play music: piano, flute, accordion, and now trying to learn classical guitar.</p> */}
-          {/* <p>📚 I love reading, everything including non-fiction, fiction, poetry, biography, memoir, history, and science</p> */}
+    <div className={classnames()}>
+      <hr style={{marginTop: `3rem`, marginBottom: `3rem`}}></hr>
+      <div id="faded_name">Hi, I'm Yennie</div>
+
+      <div className="body-container">
+        <div>
+          <p>
+            I recently completed my Master of Science (MSc) in <a href="https://www.ox.ac.uk/admissions/graduate/courses/msc-social-data-science" className="normalLink" target="_blank" rel="noreferrer">Social Data Science</a> at the Oxford Internet Institute. Currently, I am working as an AI and Data Science Research Fellow with the <a href="https://www.unglobalpulse.org/" target="_blank" className="normalLink" rel="noreferrer">UN Global Pulse</a>, where I am analyzing content shared on public radio across the African continent and <a href="https://www.unglobalpulse.org/2021/05/who-and-un-global-pulse-are-building-a-social-listening-radio-tool-to-aid-the-covid-19-infodemic-response/" target="_blank" className="normalLink" rel="noreferrer">developing social listening tools</a> to support monitoring the COVID-19 infodemic. 
+          </p>
+          <p>
+            I am a part of the <a href="http://bigdatastudies.net/" className="normalLink" target="_blank" rel="noreferrer">Big Data Studies Lab</a> at Seoul National University (SNU) as well as a student researcher with the <a href="https://oxai.org/labs" target="_blank" className="normalLink" rel="noreferrer">Oxford AI Labs</a>.
+            Before that, I was a software engineer at Microsoft on the Education team working on <a href="https://www.microsoft.com/en-us/education" className="normalLink" target="_blank" rel="noreferrer">education technology platforms</a> to empower students and teachers. Before that, I graduated from Tufts University with a B.S. in computer science, B.A. in history, and minor in music. In my spare time, I love to write creative fiction/non-fiction, learn new instruments, and combine movements ranging from martial arts to dancing.
+          </p>
         </div>
+        <div>
+          <div className="homeFlex-image">
+            <Img fluid={data.file.childImageSharp.fluid} alt="This is a picture of my face" />
+          </div>
+        </div>
+       
       </div>
+
     </div>
     <br/>
     <br/>
