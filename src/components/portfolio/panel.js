4521fbf4ae7a966e893d222d90da603ba52c2dc2
Yennie Jun <yennie.jun@gmail.com>
2022-07-18 10:58:35 -0400

    replace native tooltip wtih react tooltip

--- src/components/portfolio/panel.js
+++ src/components/portfolio/panel.js
@@ -2,12 +2,12 @@
 import React from "react"
 import Img from 'gatsby-image';
 import * as panelStyles from "./panel.module.css"
-import * as linkStyles from "./../posts/post-link.module.css"
 import { FaGithub } from 'react-icons/fa'
 import { IoIosPaper } from 'react-icons/io'
 import { RiArticleLine } from 'react-icons/ri'
 import { GiClick } from 'react-icons/gi'
-import { GoTag } from 'react-icons/go'
+import ReactTooltip from "react-tooltip";
+
 
 const PortfolioPanel = (props) => {
     return (
@@ -25,17 +25,9 @@ const PortfolioPanel = (props) => {
 			</div>
 			} 
             
-			{/* <div className={panelStyles.projectContentContainer}> */}
 			<div className={panelStyles.projectContentContainer}>
 				<h2 className={panelStyles.portfolio_panel_title}>{props.data.title}</h2> 
 
-				{props.data.tags && 
-					<div>{props.data.tags.map((item, i) => 
-					<div key={i} className={panelStyles.blogtag}>
-						<GoTag/>{item}
-					</div>)}
-					</div>
-				}
 
 				{props.data.description && 
 				 	props.data.description.map((item, i) =>
@@ -43,42 +35,39 @@ const PortfolioPanel = (props) => {
 					)
 				} 
 
-				{props.data.announcement && 
-					<h3>{props.data.announcement}</h3>
-					}
-
-				
-            
-            {/* <hr/> */}
-            
-
-
             <div className={panelStyles.codelinkContainer}>
 				{props.data.codelink &&
-					<a className={panelStyles.codelink} href={props.data.codelink} target="_blank" rel="noreferrer" title="Link to code">
+					<a data-tip data-for="githubIcon" className={panelStyles.codelink} href={props.data.codelink} target="_blank" rel="noreferrer">
 					<FaGithub/>
-					{/* <span>Link to code</span> */}
+					<ReactTooltip id="githubIcon" place="bottom" effect="solid">
+						Link to code
+					</ReactTooltip>
 					</a>
 				} 
-
 				{props.data.paperlink &&
-					<a className={panelStyles.codelink} href={props.data.paperlink} target="_blank" rel="noreferrer" title="Link to paper">
+					<a data-tip data-for="paperIcon" className={panelStyles.codelink} href={props.data.paperlink} target="_blank" rel="noreferrer">
 					<IoIosPaper />
-					{/* <span>Link to paper</span> */}
+					<ReactTooltip id="paperIcon" place="bottom" effect="solid">
+						Link to paper
+					</ReactTooltip>
 					</a>
 				}
 
 				{props.data.articlelink &&
-					<a className={panelStyles.codelink} href={props.data.articlelink} target="_blank" rel="noreferrer" title="Link to article">
-						<RiArticleLine />
-						{/* <span>Link to article</span> */}
+					<a data-tip data-for="articleIcon" className={panelStyles.codelink} href={props.data.articlelink} target="_blank" rel="noreferrer">
+					<RiArticleLine />
+					<ReactTooltip id="articleIcon" place="bottom" effect="solid">
+						Link to article
+					</ReactTooltip>
 					</a>
 					}
 
 				{props.data.demolink &&
-					<a className={panelStyles.codelink} href={props.data.demolink} target="_blank" rel="noreferrer" title="Link to demo">
+					<a data-tip data-for="demoIcon" className={panelStyles.codelink} href={props.data.demolink} target="_blank" rel="noreferrer">
 					<GiClick />
-					{/* <span>Link to demo</span> */}
+					<ReactTooltip id="demoIcon" place="bottom" effect="solid">
+						Link to demo
+					</ReactTooltip>
 					</a>
 				}
             </div>
