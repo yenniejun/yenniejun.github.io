d9cbf28a924dff25a0f55c9dc3fbd0f9a62e35dd
Yennie Jun <yennie.jun@gmail.com>
2022-07-19 21:24:15 -0400

    update social media to have twitter

--- src/components/socialmedia.js
+++ src/components/socialmedia.js
@@ -5,7 +5,7 @@ import { GrFacebookOption } from 'react-icons/gr'
 import { TiSocialLinkedin } from 'react-icons/ti'
 import { RiGithubLine } from 'react-icons/ri'
 import { AiOutlineMail} from 'react-icons/ai' 
-
+import { FiTwitter} from 'react-icons/fi' 
 import "./style/socialmedia.css"
 
 
@@ -17,21 +17,25 @@ const SocialMediaLinks = ({ location }) => {
                 <AiOutlineMail/>
                 <span>Mail</span>
             </a>
-            <a href="https://www.linkedin.com/in/yennie-jun-30a650a5/" target="_blank" rel="noreferrer">
-                <TiSocialLinkedin/>
-                <span>LinkedIn</span>
-            </a>
             <a href="https://github.com/yenniejun" target="_blank" rel="noreferrer">
                 <RiGithubLine/>
                 <span>GitHub</span>
             </a>
-            <a href="https://www.facebook.com/yennie.jun" target="_blank" rel="noreferrer">
+            {/* <a href="https://www.facebook.com/yennie.jun" target="_blank" rel="noreferrer">
                 <GrFacebookOption/>
                 <span>Facebook</span>
-            </a>
+            </a> */}
             <a href="https://www.instagram.com/_yenniejun_/" target="_blank" rel="noreferrer">
                 <FaInstagram/>
                 <span>Instagram</span>
+            </a>
+            <a href="https://twitter.com/YennieJun" target="_blank" rel="noreferrer">
+                <FiTwitter/>
+                <span>Twitter</span>
+            </a>
+            <a href="https://www.linkedin.com/in/yennie-jun-30a650a5/" target="_blank" rel="noreferrer">
+                <TiSocialLinkedin/>
+                <span>LinkedIn</span>
             </a>
         </div>
       </>
