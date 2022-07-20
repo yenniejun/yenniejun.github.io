3973486836e8c14875251870129f03781d1bad34
Yennie Jun <yennie.jun@gmail.com>
2022-07-19 21:36:25 -0400

    update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,14 +31,13 @@ const IndexPage = () => {
       
       
         <div>
-          <p>
-            I am a machine learning engineer at <a href="https://www.truveta.com/" target="_blank" className="normalLink" rel="noreferrer">Truveta</a>, a company dedicated to saving lives with data in the healthcare space. I have worked with OpenAI as a Red Team Consultant to test <a href="https://openai.com/dall-e-2/" target="_blank" className="normalLink" rel="noreferrer">DALLE-2</a> prior to its release for potential harms, biases, and disinformation.</p>
-            
-            <p>Professionally, my career has focused around applications of AI/ML for good.  Previously, I have worked as a data scientist with the United Nations on COVID-19 public health monitoring on radio; as a researcher with <a href="http://bigdatastudies.net/" className="normalLink" target="_blank" rel="noreferrer">Big Data Studies Lab</a> at Seoul National University on the future of big data archives; and as a software engineer with Microsoft on the Education team working on <a href="https://www.microsoft.com/en-us/education" className="normalLink" target="_blank" rel="noreferrer">education technology platforms</a> to empower students and teachers.</p>
+          <p>I am currently a machine learning engineer at <a href="https://www.truveta.com/" target="_blank" className="normalLink" rel="noreferrer">Truveta</a>, a company dedicated to saving lives with data in the healthcare space.</p>
 
-          <p> I have a Masters in Social Data Science from the University of Oxford and a Bachelors in computer science and history from Tufts University. I have lived in an assortment of places, including Boston, Louisiana, Seattle, Seoul, Oxford, and NYC.</p>
+          <p>My professional career has focused around applications of AI/ML for good. Previously, I have worked as a data scientist with the United Nations on COVID-19 public health monitoring on radio; as a researcher with <a href="http://bigdatastudies.net/" className="normalLink" target="_blank" rel="noreferrer">Big Data Studies Lab</a> at Seoul National University on the future of big data archives; and as a software engineer with Microsoft working on <a href="https://www.microsoft.com/en-us/education" className="normalLink" target="_blank" rel="noreferrer">education technology platforms</a> to empower students and teachers. I have also worked with OpenAI and Oxford on research to reduce harms and biases in AI.</p> 
 
-          <p>In my spare time, you can find me writing, hiking, cycling, learning to play the accordion, and practicing martial arts.</p>
+          <p>I have a Masters in Social Data Science from the University of Oxford and a Bachelors in computer science and history from Tufts University. I have lived in an assortment of places, including Boston, Louisiana, Seattle, Seoul, Oxford, and NYC.</p>
+
+          <p>In my spare time, I like to write.</p>
 
           {/* <p>You can download my resume <a className="normalLink" href={resume} target="_blank" rel="noreferrer">here</a>.</p>    */}
 
