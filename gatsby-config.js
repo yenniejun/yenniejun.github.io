9580d124bf8ecddb633007f5f3e1a7b56612d3f1
Yennie Jun <yennie.jun@gmail.com>
2022-12-01 00:04:29 -0500

    adding correct google tracking id?

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
+          "G-DPYF68J885",
           "UA-167954666-1", // Google Analytics / GA
           // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
           // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
